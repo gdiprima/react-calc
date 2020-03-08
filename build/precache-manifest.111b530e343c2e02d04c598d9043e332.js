79298816d8003b2e2040bc50132e82b8c60b7dfa
giuseppediprima <giuseppe.di.prima@me.com>
2020-03-08 11:39:21 +0100

    Added build

---
+++ build/precache-manifest.111b530e343c2e02d04c598d9043e332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4f985bfe09fbcc8116d496bee4052f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36e90a9918b63859e9ec",
+    "url": "/static/css/main.35aafc2f.chunk.css"
+  },
+  {
+    "revision": "fb69f381e84852f084bb",
+    "url": "/static/js/2.4b0425f6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4b0425f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e90a9918b63859e9ec",
+    "url": "/static/js/main.cc7dc25b.chunk.js"
+  },
+  {
+    "revision": "9411b51a84f4ced215d7",
+    "url": "/static/js/runtime-main.3003005d.js"
+  }
+]);
